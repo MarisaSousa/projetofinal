9b14117d4618028a164b23ae5f6b5d7f788d6889
Marisasousa <Marisasousa>
2019-12-09 15:00:27 -0300

    editei arquivos

--- contatos.php
+++ contatos.php
@@ -10,7 +10,7 @@ echo "
 
         <div class='section-header'>
           <h3>Contatos</h3>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+          <p>Mais informações sobre nós,entre em contato conosco:</p>
         </div>
 
         <div class='row contact-info'>
@@ -18,15 +18,15 @@ echo "
           <div class='col-md-4'>
             <div class='contact-address'>
               <i class='ion-ios-location-outline'></i>
-              <h3>Address</h3>
-              <address>A108 Adam Street, NY 535022, USA</address>
+              <h3>Endereço</h3>
+              <address>Sítio Escuro,Ce371-Acopiara Ceará</address>
             </div>
           </div>
 
           <div class='col-md-4'>
             <div class='contact-phone'>
               <i class='ion-ios-telephone-outline'></i>
-              <h3>Phone Number</h3>
+              <h3>Numero de Telefone</h3>
               <p><a href='tel:+155895548855'>+1 5589 55488 55</a></p>
             </div>
           </div>
