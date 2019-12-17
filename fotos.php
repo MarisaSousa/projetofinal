5395a5f8c3c24e4f373feeea7f5f1c093c9becf0
DanHusker <danielcastelle18@gmail.com>
2019-12-17 17:55:36 -0200

    Mudei arquivos
    mudei arquivos

--- fotos.php
+++ fotos.php
@@ -14,10 +14,10 @@ echo "
         <div class='row'>
           <div class='col-lg-12'>
             <ul id='portfolio-flters'>
-              <li data-filter='*' class='filter-active'>Todos</li>
-              <li data-filter='.filter-app'>Panificação</li>
-              <li data-filter='.filter-card'>Bolos</li>
-              <li data-filter='.filter-web'>Lanchonete</li>
+              <li data-filter='*' class='filter-active'>All</li>
+              <li data-filter='.filter-app'>App</li>
+              <li data-filter='.filter-card'>Card</li>
+              <li data-filter='.filter-web'>Web</li>
             </ul>
           </div>
         </div>
@@ -28,12 +28,12 @@ echo "
             <div class='portfolio-wrap'>
               <figure>
                 <img src='img/IMG-20191216-WA0141.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/app1.jpg' data-lightbox='portfolio' data-title='App 1' class='link-preview' title='Preview'><i class='ion ion-eye'></i></a>
+                <a href='img/IMG-20191216-WA0141.jpg' data-lightbox='portfolio' data-title='App 1' class='link-preview' title='Preview'><i class='ion ion-eye'></i></a>
                 <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
               </figure>
 
               <div class='portfolio-info'>
-                <h4><a href='#'>App 1</a></h4>
+                <h4><a href='#'>Pão Bola</a></h4>
                 <p>App</p>
               </div>
             </div>
@@ -42,13 +42,13 @@ echo "
           <div class='col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp' data-wow-delay='0.1s'>
             <div class='portfolio-wrap'>
               <figure>
-                <img src='img/portfolio/web3.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/web3.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 3' title='Preview'><i class='ion ion-eye'></i></a>
+                <img src='img/IMG-20191216-WA0142.jpg' class='img-fluid' alt=''>
+                <a href='img/IMG-20191216-WA0142.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 3' title='Preview'><i class='ion ion-eye'></i></a>
                 <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
               </figure>
 
               <div class='portfolio-info'>
-                <h4><a href='#'>Web 3</a></h4>
+                <h4><a href='#'>Pães Recheados</a></h4>
                 <p>Web</p>
               </div>
             </div>
@@ -57,43 +57,30 @@ echo "
           <div class='col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp' data-wow-delay='0.2s'>
             <div class='portfolio-wrap'>
               <figure>
-                <img src='img/portfolio/app2.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/app2.jpg' class='link-preview' data-lightbox='portfolio' data-title='App 2' title='Preview'><i class='ion ion-eye'></i></a>
+                <img src='img/IMG-20191216-WA0144.jpg' class='img-fluid' alt=''>
+                <a href='img/IMG-20191216-WA0144.jpg' class='link-preview' data-lightbox='portfolio' data-title='App 2' title='Preview'><i class='ion ion-eye'></i></a>
                 <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
               </figure>
 
               <div class='portfolio-info'>
-                <h4><a href='#'>App 2</a></h4>
+                <h4><a href='#'>Pães</a></h4>
                 <p>App</p>
               </div>
             </div>
           </div>
 
-          <div class='col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp'>
-            <div class='portfolio-wrap'>
-              <figure>
-                <img src='img/portfolio/card2.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/card2.jpg' class='link-preview' data-lightbox='portfolio' data-title='Card 2' title='Preview'><i class='ion ion-eye'></i></a>
-                <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
-              </figure>
-
-              <div class='portfolio-info'>
-                <h4><a href='#'>Card 2</a></h4>
-                <p>Card</p>
-              </div>
-            </div>
-          </div>
+          
 
           <div class='col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp' data-wow-delay='0.1s'>
             <div class='portfolio-wrap'>
               <figure>
-                <img src='img/portfolio/web2.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/web2.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 2' title='Preview'><i class='ion ion-eye'></i></a>
+                <img src='img/IMG-20191216-WA0147.jpg' class='img-fluid' alt=''>
+                <a href='img/IMG-20191216-WA0147.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 2' title='Preview'><i class='ion ion-eye'></i></a>
                 <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
               </figure>
 
               <div class='portfolio-info'>
-                <h4><a href='#'>Web 2</a></h4>
+                <h4><a href='#'>Carrioquinha</a></h4>
                 <p>Web</p>
               </div>
             </div>
@@ -102,59 +89,14 @@ echo "
           <div class='col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp' data-wow-delay='0.2s'>
             <div class='portfolio-wrap'>
               <figure>
-                <img src='img/portfolio/app3.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/app3.jpg' class='link-preview' data-lightbox='portfolio' data-title='App 3' title='Preview'><i class='ion ion-eye'></i></a>
+                <img src='img/IMG-20191216-WA0146.jpg' class='img-fluid' alt=''>
+                <a href='img/IMG-20191216-WA0146.jpg' class='link-preview' data-lightbox='portfolio' data-title='App 3' title='Preview'><i class='ion ion-eye'></i></a>
                 <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
               </figure>
 
               <div class='portfolio-info'>
-                <h4><a href='#'>App 3</a></h4>
+                <h4><a href='#'>Pão Carteiro</a></h4>
                 <p>App</p>
-              </div>
-            </div>
-          </div>
-
-          <div class='col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp'>
-            <div class='portfolio-wrap'>
-              <figure>
-                <img src='img/portfolio/card1.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/card1.jpg' class='link-preview' data-lightbox='portfolio' data-title='Card 1' title='Preview'><i class='ion ion-eye'></i></a>
-                <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
-              </figure>
-
-              <div class='portfolio-info'>
-                <h4><a href='#'>Card 1</a></h4>
-                <p>Card</p>
-              </div>
-            </div>
-          </div>
-
-          <div class='col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp' data-wow-delay='0.1s'>
-            <div class='portfolio-wrap'>
-              <figure>
-                <img src='img/portfolio/card3.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/card3.jpg' class='link-preview' data-lightbox='portfolio' data-title='Card 3' title='Preview'><i class='ion ion-eye'></i></a>
-                <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
-              </figure>
-
-              <div class='portfolio-info'>
-                <h4><a href='#'>Card 3</a></h4>
-                <p>Card</p>
-              </div>
-            </div>
-          </div>
-
-          <div class='col-lg-4 col-md-6 portfolio-item filter-web wow fadeInUp' data-wow-delay='0.2s'>
-            <div class='portfolio-wrap'>
-              <figure>
-                <img src='img/portfolio/web1.jpg' class='img-fluid' alt=''>
-                <a href='img/portfolio/web1.jpg' class='link-preview' data-lightbox='portfolio' data-title='Web 1' title='Preview'><i class='ion ion-eye'></i></a>
-                <a href='#' class='link-details' title='More Details'><i class='ion ion-android-open'></i></a>
-              </figure>
-
-              <div class='portfolio-info'>
-                <h4><a href='#'>Web 1</a></h4>
-                <p>Web</p>
               </div>
             </div>
           </div>
